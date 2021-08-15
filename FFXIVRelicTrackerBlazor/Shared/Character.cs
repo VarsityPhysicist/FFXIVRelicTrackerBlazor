73ced3e9ede68a90388db2066324f5a034f6881b
Justin Lata <latajustin@gmail.com>
2021-08-14 21:37:20 -0400

    Added HW expansion

--- FFXIVRelicTrackerBlazor/Shared/Character.cs
+++ FFXIVRelicTrackerBlazor/Shared/Character.cs
@@ -1,4 +1,5 @@
 ﻿using FFXIVRelicTrackerBlazor.Shared._2_Arr;
+using FFXIVRelicTrackerBlazor.Shared._3_HW;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace FFXIVRelicTrackerBlazor.Shared
         public Character()
         {
             ArrExpansion = new ArrExpansion();
+            HWExpansion = new HWExpansion();
             Name = "Default Character";
         }
         public Character(Character oldCharacter)
@@ -21,7 +23,11 @@ namespace FFXIVRelicTrackerBlazor.Shared
 
             if (oldCharacter.ArrExpansion == null) this.ArrExpansion = new ArrExpansion();
             else this.ArrExpansion = oldCharacter.ArrExpansion;
+
+            if (oldCharacter.HWExpansion == null) this.HWExpansion = new HWExpansion();
+            else this.HWExpansion = oldCharacter.HWExpansion;
         }
         public ArrExpansion ArrExpansion { get; set; }
+        public HWExpansion HWExpansion { get; set; }
     }
 }
