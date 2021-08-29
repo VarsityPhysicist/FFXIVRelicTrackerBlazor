0502383b9c98d3a8f5dcb4819d977d4435aae78a
Justin Lata <latajustin@gmail.com>
2021-08-29 09:47:58 -0400

    Added HWStageCompleter reference Added optional bool to adjust counts of items

--- FFXIVRelicTrackerBlazor/Shared/Helpers/Misc/MasterStageHelper.cs
+++ FFXIVRelicTrackerBlazor/Shared/Helpers/Misc/MasterStageHelper.cs
@@ -10,20 +10,26 @@ namespace FFXIVRelicTrackerBlazor.Shared.Helpers
 {
     public static class MasterStageHelper
     {
-        public static void CompleteStage(Character character, JobName Job, int StageIndex, ExpansionName expansionName)
+        public static void CompleteStage(Character character, JobName Job, int StageIndex, ExpansionName expansionName, bool adjustCounts = true)
         {
+
             switch (expansionName)
             {
                 case ExpansionName.Arr:
-                    ArrStageCompleter.CompleteStage(Job, StageIndex, character.ArrExpansion);
+                    ArrStageCompleter.CompleteStage(Job, StageIndex, character.ArrExpansion, adjustCounts);
                     break;
                 case ExpansionName.HW:
+                    HWStageCompleter.CompleteStage(Job, StageIndex, character.HWExpansion, adjustCounts);
                     break;
                 case ExpansionName.SB:
                     break;
                 case ExpansionName.ShB:
                     break;
             }
+
+            StageInfo targetStage = character.GetExpansions().Single(x => x.Expansion == expansionName).GetStages().Single(x => x.StageIndex == StageIndex);
+            JobName activeJob = targetStage.ActiveJob;
+            if (activeJob == Job) targetStage.ActiveJob = JobName.NA;
         }
         public static void InCompleteStage(Character character, JobName Job, int StageIndex, ExpansionName expansionName)
         {
@@ -33,6 +39,7 @@ namespace FFXIVRelicTrackerBlazor.Shared.Helpers
                     ArrStageCompleter.InCompleteStage(Job, StageIndex, character.ArrExpansion);
                     break;
                 case ExpansionName.HW:
+                    HWStageCompleter.InCompleteStage(Job, StageIndex, character.HWExpansion);
                     break;
                 case ExpansionName.SB:
                     break;
@@ -49,6 +56,7 @@ namespace FFXIVRelicTrackerBlazor.Shared.Helpers
                     ArrStageResetter.ResetArrStage(StageIndex, character.ArrExpansion);
                     break;
                 case ExpansionName.HW:
+                    HWStageResetter.ResetStage(StageIndex, character.HWExpansion);
                     break;
                 case ExpansionName.SB:
                     break;
