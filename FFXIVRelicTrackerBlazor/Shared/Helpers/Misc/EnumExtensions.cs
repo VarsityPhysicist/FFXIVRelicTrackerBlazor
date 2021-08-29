732db7a530b562e56c4682a258ca673466cbafce
Justin Lata <latajustin@gmail.com>
2021-08-29 16:35:49 -0400

    Moved to shared

--- FFXIVRelicTrackerBlazor/Shared/Helpers/Misc/EnumExtensions.cs
+++ FFXIVRelicTrackerBlazor/Shared/Helpers/Misc/EnumExtensions.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 
-namespace FFXIVRelicTrackerBlazor.Client.Extensions
+namespace FFXIVRelicTrackerBlazor.Shared.Extensions
 {
     public static class EnumExtensions
     {
