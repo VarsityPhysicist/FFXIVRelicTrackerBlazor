3fe710b2c6a9b8d6aec037f0cb466d1608336995
Justin Lata <latajustin@gmail.com>
2021-10-09 15:43:09 -0400

    Added Skysteel tools to main function

--- FFXIVRelicTrackerBlazor/Shared/Helpers/Misc/WeaponNames.cs
+++ FFXIVRelicTrackerBlazor/Shared/Helpers/Misc/WeaponNames.cs
@@ -19,6 +19,8 @@ namespace FFXIVRelicTrackerBlazor.Shared.Helpers.Misc
                     return GetHWWeapon(job, StageIndex);
                 case ExpansionName.ShB:
                     return GetShBWeapon(job, StageIndex);
+                case ExpansionName.Skysteel:
+                    return GetSkysteelWeapon(job, StageIndex);
                 default:
                     return null;
             }
